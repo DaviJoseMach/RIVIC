4ac2be72a20b6b69ef0e7bdc6e69982f6f96a0f1
LeviAckr <thech.nasa.dj@gmail.com>
2023-07-27 13:27:31 -0300

    adicionando o restante no anotes

--- anotes/loop.rs
+++ anotes/loop.rs
@@ -1,3 +1,13 @@
 fn main(){
-    
+    let mut counter = 0;
+
+    loop {
+        println!("O contador é: {}", counter);
+        counter += 1;
+
+        if counter == 5 {
+            break; // Interrompe o loop quando o contador atinge 5
+        }
+    }
+
 }
--- anotes/struct.rs
+++ anotes/struct.rs
@@ -1,3 +1,16 @@
+struct Pessoa {
+    nome: String,
+    idade: u32,
+}
+
 fn main(){
-    
+     // Criando uma instância da struct "Pessoa"
+     let pessoa1 = Pessoa {
+        nome: String::from("Alice"),
+        idade: 30,
+    };
+
+    // Acessando os campos da struct
+    println!("Nome: {}", pessoa1.nome);
+    println!("Idade: {}", pessoa1.idade);
 }
